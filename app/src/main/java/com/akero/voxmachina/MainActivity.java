9f06afb8537b1eaea58777f1b8c2a546c61b4ee5
Akero <rish1994@gmail.com>
2023-09-20 12:51:39 +0530

    notification is not showing up

--- app/src/main/java/com/akero/voxmachina/MainActivity.java
+++ app/src/main/java/com/akero/voxmachina/MainActivity.java
@@ -1,25 +1,126 @@
 package com.akero.voxmachina;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
+import androidx.work.PeriodicWorkRequest;
+import androidx.work.WorkManager;
+
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PERMISSION_REQUEST_CODE = 1234;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        // Initialize your UI elements and other components here
-        initViews();
+        // Create the notification channel
+        createNotificationChannel();
+        checkAndRequestPermissions();
     }
 
-    private void initViews() {
-        // For example:
-        // Button myButton = findViewById(R.id.my_button);
-        // myButton.setOnClickListener(view -> {
-        //     // Handle button click
-        // });
+    private void createNotificationChannel() {
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("notifyMe", "Notification Channel", NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
     }
+
+
+    private void checkAndRequestPermissions() {
+        Log.d("tag1", "in checkpermissions");
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.FOREGROUND_SERVICE) != PackageManager.PERMISSION_GRANTED) {
+
+            Log.d("tag1", "checkreqperm asking for perm");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.FOREGROUND_SERVICE}, PERMISSION_REQUEST_CODE);
+        } else {
+            Log.d("tag1", "checkreqperm permission already granted");
+            // If the app already has the permission, proceed with whatever you want to do next.
+            onPermissionAlreadyGranted();
+            //TODO for testing remove this and implement properly
+            sendNotification();  // Send the notification immediately
+            scheduleReminder();
+        }
+    }
+
+    private void onPermissionAlreadyGranted() {
+        Log.d("tag1", "onPermissionAlreadyGranted");
+
+        // Implement the next steps you want to perform after verifying the permission is granted.
+        // For now, I'm placing your notification and reminder methods here:
+        sendNotification();  // Send the notification immediately for testing purposes
+        scheduleReminder();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Permission granted, schedule the reminder
+                Log.d("tag1", "permission granted");
+
+                scheduleReminder();
+            } else {
+
+                Log.d("tag1", "permission not granted");
+                // Handle the permission denial according to your app's user experience
+                // Maybe show a message explaining why the permission is crucial.
+            }
+        }
+    }
+
+    private void scheduleReminder() {
+        PeriodicWorkRequest reminderRequest =
+                new PeriodicWorkRequest.Builder(ReminderWorker.class, 24, TimeUnit.HOURS)
+                        .build();
+
+        WorkManager.getInstance(this).enqueue(reminderRequest);
+    }
+
+    void sendNotification() {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(getApplicationContext(), "notifyMe")
+                .setSmallIcon(R.drawable.ic_launcher_foreground)
+                .setContentTitle("Reminder")
+                .setContentText("This is your reminder!")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        Log.d("tag2", "1");
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(getApplicationContext());
+
+        Log.d("tag2", "2");
+
+        // Since there isn't a runtime permission for posting notifications,
+        // we can directly notify without checking for permissions.
+        Log.d("tag2", "4");
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        notificationManager.notify(1, builder.build());
+    }
+
 }
---
+++ app/src/main/java/com/akero/voxmachina/ReminderWorker.java
@@ -0,0 +1,29 @@
+package com.akero.voxmachina;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.akero.voxmachina.R;
+
+public class ReminderWorker extends Worker {
+
+    public ReminderWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        // This method runs on a background thread
+        return Result.success();
+    }
+
+
+}
